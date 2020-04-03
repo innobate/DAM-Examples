d97b4307df6dbf834f74f4077daec877330068c9
Shelvin Datt <Shelvin.Datt@private-flight.com>
2020-04-03 19:28:37 +1300

    WPF example

---
+++ WPF Example CSV/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Example_CSV
+{
+    public class Country
+    {
+        public double id { get; set; }
+        public string name { get; set; }
+        public string alpha2 { get; set; }
+        public string alpha3 { get; set; }
+    }
+}
---
+++ WPF Example CSV/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DAM;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Example_CSV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetGridDataSource();
+        }
+
+        private void SetGridDataSource()
+        {
+            // Connection string for a Text file
+            string ConnectionString = @"Driver={Microsoft Excel Driver (*.xls)};DriverId=790;DBQ=" + AppDomain.CurrentDomain.BaseDirectory + @"world-countries\data\en\world.xls";
+
+            using (var dbManager = new DataAccessManager(OdbcFactory.Instance, ConnectionString))
+            {
+                var y = dbManager.Select<Country>("Select * from [world$]");
+                dgTest.SetValue(ListView.ItemsSourceProperty, y);
+            }
+        }
+    }
+}
