e807932ae34d68f79d0e37a4b2630cd777b34477
Shelvin Datt <Shelvin.Datt@private-flight.com>
2020-04-03 21:07:10 +1300

    Framework .NET 4 example

---
+++ ConsoleApp NET40/Program.cs
@@ -0,0 +1,32 @@
+﻿using DAM;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            var dbManager = new DataAccessManager("System.Data.SqlClient", connection);
+            var countries = dbManager.Select<Country>("select * from Country");
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine("Country: {0}  - Alpha-2: {1}   - Alpha-3: {2}", country.name, country.alpha2, country.alpha3);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Country
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string alpha2 { get; set; }
+        public string alpha3 { get; set; }
+    }
+}
